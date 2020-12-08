1653c0f186d76b02f5a10a91daf0aa544f766416
YEN-CHEN CHOU <YEN-CHEN.CHOU@GRAINGER.COM>
2020-12-09 03:47:05 +0800

    update Docker/README.md

---
+++ DataStructure_Algorithm/Python/selection_sort.py
@@ -0,0 +1,4 @@
+# The central idea of mergesort
+# The central tricks of space complexity is to see the depth of the call stack. Remember each stack store all the
+# local information of the function. So the space complexity is depend on the length of the call stack and space
+# occupied during each call stack
