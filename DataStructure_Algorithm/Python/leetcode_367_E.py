6fa7993b88da964272412fcbba2e500c6e295fd1
Yen-Chen Chou <intelchou@gmail.com>
2021-03-22 22:54:43 -0500

    Update leetcode_367_E.py

--- DataStructure_Algorithm/Python/leetcode_367_E.py
+++ DataStructure_Algorithm/Python/leetcode_367_E.py
@@ -17,3 +17,20 @@ class Solution:
         return False
     #O(logn) O(1)
 
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num < 2: return num
+        left, right = 1, num
+        while left <= right:
+            mid = (right-left)//2+left
+            val = mid * mid
+            if val == num:
+                return True
+            elif val > num:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return False
+    #O(logn) O(1)
+
