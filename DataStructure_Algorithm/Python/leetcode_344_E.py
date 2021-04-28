70ad336b54ef970fae56fbfa898a152751f8e024
YEN-CHEN CHOU <YEN-CHEN.CHOU@GRAINGER.COM>
2021-04-27 21:49:13 -0500

    Update leetcode_344_E.py

--- DataStructure_Algorithm/Python/leetcode_344_E.py
+++ DataStructure_Algorithm/Python/leetcode_344_E.py
@@ -26,4 +26,25 @@ class Solution:
             right -= 1
             left += 1
     # O(n), O(1)
-        
+
+# Solution 3: Recursion
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        return self.helper(s, 0, len(s)-1)
+
+    def helper(self, left, right):
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            return self.helper(s, left+1, right-1)
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        def helper(left, right):
+            if left < right:
+                s[right], s[left] = s[left], s[right]
+                helper(left+1, right-1)
+        helper(0, len(s)-1)  # [1, 2]
